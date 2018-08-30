5052a9382d33479cc070f5aa5e5a96084e101cb0
Harsh Agarwal <cs15btech11019@iith.ac.in>
2018-08-30 19:28:20 +0530

    handle comments and find frequency chapter wise

--- Assignment-2/src/task.py
+++ Assignment-2/src/task.py
@@ -19,15 +19,22 @@ from matplotlib import pyplot as plt
 p = AP()
 p.add_argument('--datasrc', type=str, required=True,
                             help='Directory where the documents are stored. Files to be presented in TXT format')
-p.add_argument('--top_k', type=int, required=True,
-               help='How many words to consider for analysis')
+p.add_argument('--top_k', type=int, default=20,
+               help='number of top words for the analysis')
+p.add_argument('--top_k_scheme', default='frequency',
+               choices=['frequency', 'entropy', 'custom'], 
+               help='scheme to use to find top_k words for the analysis')
+p.add_argument('--chapter_wise', action='store_true',
+               help='Toggle to perform the analysis chapter wise')
 p.add_argument('--log_scale', action='store_true',
                               help='Toggle to generate graphs in log-scale')
-p.add_argument('--linear_fit', action='store_true',
-                               help='Toggle to compute the linear fit')
+
 p = p.parse_args()
 
+analysis_type = "chapter" if p.chapter_wise else "complete book"
 top_k = p.top_k
+top_k_scheme = p.top_k_scheme
+
 # bar format for tqdm progress bar
 BAR_FORMAT = '{desc}{percentage:3.0f}% | {bar} | {n_fmt}/{total_fmt}'
 
@@ -53,7 +60,11 @@ ALL_STOPWORDS = set(stopwords.words('english'))  # Set is faster because set use
 stemmer = PorterStemmer()
 lemmatizer = WordNetLemmatizer()
 
-all_terms = []
+if analysis_type == "chapter":
+    chapter_term_frequency = [[]] * len(doc_db_filepath)
+    chapter_counter = []
+
+book_term_frequency = []
 all_warns = 0
 for doc_id in tqdm(range(len(doc_db_filepath)), ascii=True, desc='processing', bar_format=BAR_FORMAT):
     with open(doc_db_filepath[doc_id], "r") as file:
@@ -70,34 +81,27 @@ for doc_id in tqdm(range(len(doc_db_filepath)), ascii=True, desc='processing', b
         tmp = [lemmatizer.lemmatize(tmp_w) for tmp_w in tmp]  # Lemmatize
 
         tmp = [tmp_w for tmp_w in tmp if tmp_w != '']  # Stemming and Lemmatization can cause empty string results
-        all_terms += tmp
+        if analysis_type == "chapter":
+            chapter_term_frequency[doc_id] = chapter_term_frequency[doc_id] + tmp
+        
+        book_term_frequency += tmp
 
-all_counter = Counter(all_terms)
 if all_warns != 0:
     warnings.warn("There were {} empty documents".format(all_warns), RuntimeWarning)
 
 # Print the top k terms
-print("Top {} common terms using frequency".format(top_k))
-for mcw in all_counter.most_common(top_k):
-    print("{}: {}".format(mcw[0], mcw[1]))
+if analysis_type == "chapter":
+    for ch in chapter_term_frequency[:10]:
+        chapter_counter.append( Counter(ch))
 
-# Plot the frequency vs. rank graph
-plt.figure(figsize=(10, 8))
-plt.title('Frequency vs. Rank for the corpus: {}'.format(os.path.basename(p.datasrc)), fontsize=20)
-plt.xlabel('Rank (1 to {})'.format(len(all_counter)), fontsize=15)
-plt.ylabel('Frequency', fontsize=15)
-if p.log_scale:
-    plt.xscale('log')
-    plt.yscale('log')
-plt.plot(list(range(1, len(all_counter) + 1)), sorted(all_counter.values(), reverse=True),
-         'b-', linewidth=3.0, alpha=0.4)
-plt.scatter(list(range(1, len(all_counter) + 1)), sorted(all_counter.values(), reverse=True),
-            2.0, color='k')
-plt.tight_layout()
-plt.show()
+    for cnt in chapter_counter:
+        print("Top {} common terms using {} performed {} wise for 10 chapters.".format(
+            top_k, p.top_k_scheme, analysis_type))
+        for mcw in cnt.most_common(top_k):
+            print("{}: {}".format(mcw[0], mcw[1]))
+        print("----")
 
-if p.linear_fit:
-    xs = np.log10(np.arange(1, len(all_counter) + 1, 1))
-    ys = np.log10(np.array(sorted(all_counter.values(), reverse=True)))
-    solution = np.polyfit(xs, ys, 1)
-    print("negative slope: {}, intercept: {}".format(round(-solution[0], 3), round(solution[1], 3)))
+else:
+    all_counter = Counter(book_term_frequency)
+    for mcw in all_counter.most_common(top_k):
+        print("{}: {}".format(mcw[0], mcw[1]))
